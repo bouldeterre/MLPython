ed115d770efe080f338b00499492fa834b0a409e
bouldeterre <pierreemmanuel.emonides@gmail.com>
2018-09-06 00:10:25 +0200

    poetry init

---
+++ index.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+
+def main():
+    x = np.array([[1, 2, 3], [4, 5, 6]])
+    print("x:\n{}".format(x))
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+    
